6674d2bdd6038135bb0fb2c929d35929bd860850
jasapper <jasap98@yahoo.com>
2017-12-10 04:25:41 -0500

    Code tweaks and bootstrap

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,14 +1,14 @@
 // Initializing global variables
 var countWin = 0;
 var countLoss = 0;
-var gameOver = false;
 var gemBlue = 0;
 var gemOrange = 0;
 var gemPurple = 0;
 var gemPink = 0;
 var totalScore = 0;
+var gameOver = false;
 
-// Random number generators (number between 19-120), place number in randomNumberBox
+// Random number generators (number between 19-120)
 var targetMax = 120;
 var targetMin = 19;
 var randomNumber = Math.floor(Math.random()*(targetMax - targetMin + 1)) + targetMin;
@@ -19,7 +19,7 @@ gemOrange = Math.floor(Math.random() * 12) + 1;
 gemPurple = Math.floor(Math.random() * 12) + 1;
 gemPink = Math.floor(Math.random() * 12) + 1;	
 
-// Logic to reset game
+// Logic to reset game after win/loss
 function nextGame() {
 	randomNumber = Math.floor(Math.random()*(targetMax - targetMin + 1)) + targetMin;
 	$("#randomNumber").html(randomNumber);
@@ -37,31 +37,28 @@ function checkWin(){
 	if(totalScore === randomNumber){
 		countWin++;
 		$("#wins").html(countWin);
-		alert("That's a WIN!! " + totalScore + " = " + randomNumber);
+		alert("That's a WIN buddy!! Final score (" + totalScore + ") matches target (" + randomNumber + ").");
 		gameOver = true;
 		nextGame();
-		}
+	}
 }
 
 // Checking for LOSS (user's total score has exceeded the random target number)
 function checkLoss(){
 	if(totalScore > randomNumber){
 		countLoss++;
-	$("#lose").html(countLoss);
-	alert("Uh oh, you went OVER!! " + totalScore + " > " + randomNumber);
-	nextGame();
+		$("#lose").html(countLoss);
+		alert("Game over man, game over! Final score: " + totalScore + " ( > " + randomNumber + ")");
+		nextGame();
 	}
-	//location.reload();
 }
 
-//The document loads and the game is available for the user interact
-
-$(function(){
+$(document).ready(function(){
 	
 // Display the random target number
 $("#randomNumber").append(randomNumber);
 
-// When each crystal is clicked, the number assigned to it will be added to user's total score
+// When each crystal is clicked, the number assigned to it will be added to user's current score
 $("#gemBlue").on("click", function(){
 	totalScore += gemBlue;
 	$("#score").html(totalScore);
@@ -89,4 +86,5 @@ $("#gemPink").on("click", function(){
 	checkWin();
 	checkLoss();
 });
+// Current score reflects (actual) final score before triggering alert
 });
